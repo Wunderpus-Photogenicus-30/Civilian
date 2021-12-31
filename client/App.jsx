2ff8b034ee07338460a09c1d5ca96f7955a7394b
Chloe Lu <luchen2244@gmail.com>
2021-12-31 17:26:10 -0500

    frontend-added

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { useState, useEffect } from "react"
+
+import './stylesheets/style.css';
+
+const App = () => {
+  console.log('in app.jsx');
+  return (
+    <div id="app">
+    </div>
+  );
+};
+
+export default App;
