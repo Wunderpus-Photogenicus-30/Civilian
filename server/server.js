dcb7680100726607f82404fcb59d29ca966c8aa5
Huy <huy@Huys-iMac-Pro.local>
2021-12-31 13:58:59 -0800

    built server.js and added node and express in package

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const apiRouter = require('./routes/api');
+
+// Handels parsing request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// handle requests for static files
+app.use(express.static('./client'));
+
+const HMTL_FILE = path.resolve(__dirname, '../client/index.html');
+
+// route handler to respond with main app
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .contentType('text/html')
+    .sendFile(HTML_FILE, (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+});
+
+// defining route handlers
+app.use('/api', apiRouter);
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occured' },
+  };
+
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+
+  const errorStatuscode = errorObj.status || 500;
+
+  return res.status(errorStatusCode).json(errorObj.message);
+});
+
+// Start Server
+app.listen(Port, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
