ee3f65b2bbd1605d947dfc781dd6243111ec3fcc
Ruzeb <ruzeb1996@gmail.com>
2022-01-02 01:14:20 -0500

    adding for checkout, added css transition, added responsiveness with post responsiveness bugged

--- client/App.jsx
+++ client/App.jsx
@@ -15,15 +15,18 @@ const App = (props) => {
   console.log('in app.jsx');
   return (
     <div id="app">
-      <img id="logo" src="https://cdn-icons-png.flaticon.com/512/1476/1476778.png"/>
+      <div id="logo-wrapper">
+        <img id="logo" src="https://cdn-icons-png.flaticon.com/512/1476/1476778.png"/>
+      </div>
       <div id="main-content">
-        <div id="map">MAP HERE</div>
+        <div id="map">
+          <img id="map-img" src="https://assets.website-files.com/5e832e12eb7ca02ee9064d42/5f7db426b676b95755fb2844_Group%20805.jpg"/>
+        </div>
         <Post />
       </div>
-      {props.expandedPost && (
       <CSSTransition in={props.expandedPost} timeout={500} classNames="expanded-transition" unmountOnExit appear>
         <ExpandedPost key={1000}/>
-    </CSSTransition>)}
+      </CSSTransition>
     </div>
   );
 };
