5f506cfb4a834109fe879e5631ace6c08fc97884
Huy <huy@Huys-iMac-Pro.local>
2022-01-03 12:44:48 -0800

    added server readme, added bcrypt hashing for user password, edited some api endroutes, please check readme

--- server/README.md
+++ server/README.md
@@ -1,5 +1,28 @@
-Endpoint Routes:
+(ENDPOINTS UPDATED! CHECK YOUR AXIOS ROUTES!!!)
 
+** public.user table: **
+-- columns --
+user_id(PK) | name(req) | email | photo | password(req) *encrypted
+
+current userLogin info: name - password
+"Huy Bui" - “123”
+"Will Sentance" - “codesmith”
+"Harry Potter" - “hedwig”
+"Ron Weasley" - “scabbers”
+"Hermione Granger" - “crookshanks”
+"Remus Lupin" - “padfoot”
+
+
+
+** public.incident table: **
+-- columns --
+incident_id(PK) | street_name(req) | image_url(req) | details(req) | time | user_id | location_id | title(req) | video_url(req)
+
+1	| "4848 Rue Loiret, San Jose, CA 95136"	|	null | "Illegal fireworks going off!" |	"1/3/2022, 8:55:27 AM" | null |null |	"noise complaint"	
+
+
+
+** Endpoint Routes (UPDATED) Please check your axios requests match :D **
 'api/users'
   - gets all users from public.user table
 
@@ -10,7 +33,7 @@ Endpoint Routes:
   - gets name, photo from pulblic.user by userName
   - need to provide username and password in req.body object
   - username must be the same in public.user.name
-  - password must be the same in public.user.password
+  - bcrypt.compare will check to see if password matches hashed password in public.user.password
 
 
 'api/incidents/location/:name',
@@ -23,8 +46,8 @@ Endpoint Routes:
   - posts data into a row in the public.incident table
 
 
-'api/newuser',
-  - posts name, password into a row in the public.user table
+'api/signup',
+  - posts name, encrypted password into a row in the public.user table
 
 
 '/api/incidents/update-title:id',
--- server/api.js
+++ server/api.js
@@ -5,20 +5,24 @@ const controller = require('./controller');
 const router = express.Router();
 
 // gets all rows from public.user table
-router.get('/users', controller.getUsers, (req, res) =>
-  res.status(200).json(res.locals.userInfo)
+router.get(
+  '/users', 
+  controller.getUsers, 
+  (req, res) => res.status(200).json(res.locals.userInfo)
 );
 
 // gets all incidents from public.incident table
-router.get('/incidents', controller.getIncidents, (req, res) =>
-  res.status(200).json(res.locals.incidentInfo)
+router.get(
+  '/incidents', 
+  controller.getIncidents, 
+  (req, res) => res.status(200).json(res.locals.incidentInfo)
 );
 
-// gets name, photo from pulblic.user by userName, passing username & password in req.body
+// gets name, photo from pulblic.user, passing username &  decrypting password in req.body
 router.get(
-  '/incidents/user',
-  controller.getUserName,
-  (req, res) => res.status(200).json(res.locals.UserName)
+  '/incidents/user', 
+  controller.getUserName, 
+  (req, res) => res.status(200).json(res.locals.user)
 );
 
 // gets all incidents from public.incident by street name. Can target address, city, state, or zipcode
@@ -29,15 +33,23 @@ router.get(
 );
 
 // posts data into a row in the public.incident table
-router.post('/postevent', controller.postEvent, (req, res) =>
-  res.status(201).json(res.locals.allEvents)
+router.post(
+  '/postevent', 
+  controller.postEvent, 
+  (req, res) => res.status(201).json(res.locals.allEvents)
 );
 
-// posts name, password into a row in the public.user table
-router.post('/newuser', controller.newUser, (req, res) =>
-  res.status(201).json(res.locals.newUser)
+// posts name, encrypted password into a row in the public.user table
+router.post(
+  '/signup', 
+  controller.hash, 
+  controller.newUser, 
+  (req, res) =>res.status(201).json(res.locals.newUser)
 );
 
+
+// all these update public.incident by its relevant column. 
+// :Id should match primary key of public.incident.incident_id
 router.put(
   '/incidents/update-title:id',
   controller.updateIncidentTitle,
@@ -67,9 +79,5 @@ router.put(
   controller.updateIncidentDetails,
   (req, res) => res.status(200).json('details was updated!')
 );
-
-// router.get('/recent', (req, res) =>
-//   res.status(200).json(res.locals.recent)
-// );
 
 module.exports = router;
